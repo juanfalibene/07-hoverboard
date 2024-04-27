cc8da4d81651556eaa81095df14a12dde420e861
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-04-27 12:01:09 +0200

    variables setup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("ready");
+const container = document.getElementById("container");
+const colors = ["#D97B8B", "#163964", "#26D29B", "#7873C3", "#C6CA66"];
+const SQUARES = 500;
+
+for (i = 0; i < SQUARES; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  container.appendChild(square);
+}
